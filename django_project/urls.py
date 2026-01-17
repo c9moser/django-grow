d5437b5bd1b6f3bac3ff7fa24d62f9954ab5f4b5
Christian Moser <christian@cmoser.eu>
2026-01-17 09:36:40 +0100

    2026.01.17 09:36:40 (cachyos.cmoser.eu)

--- django_project/urls.py
+++ django_project/urls.py
@@ -18,14 +18,14 @@ from django.urls import path, include
 from django.conf import settings
 from django.contrib import admin
 from allauth.account.decorators import secure_admin_login
-
+from django.conf.urls.i18n import i18n_patterns
 
 
 urlpatterns = [
     path('accounts/', include('allauth.urls')),
     path('api/', include('grow.growapi.urls')),
-    path('', include('core.urls')),
-    path('', include('grow.urls')),
+    i18n_patterns(path('', include('core.urls'))),
+    i18n_patterns(path('', include('grow.urls'))),
 ]
 if settings.ADMIN_URL:
     urlpatterns.insert(0, path(settings.ADMIN_URL, admin.site.urls))
