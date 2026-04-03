f4555f8669ee4c953a1e79a394b95bab6f0cabd4
Christian Moser <christian@cmoser.eu>
2026-04-03 02:49:09 +0200

    2026.04.03 02:49:09 (cachyos.cmoser.eu)

--- django_project/wsgi.py
+++ django_project/wsgi.py
@@ -9,9 +9,14 @@ https://docs.djangoproject.com/en/6.0/howto/deployment/wsgi/
 from pathlib import Path
 import os
 import sys
+BASE_DIR = Path(__file__).resolve().parent.parent
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'django_project.settings')
-BASE_DIR = Path(__file__).resolve().parent.parent
+venv = os.environ.get('GROW_VENV', None)
+if venv:
+    venv_path = Path(venv).resolve() / 'lib' / f"python{sys.version_info.major}.{sys.version_info.minor}" / 'site-packages'
+    if venv_path.is_dir() and not str(venv_path) in sys.path:
+        sys.path.insert(0, str(venv_path))
 
 if not str(BASE_DIR) in sys.path:
     sys.path.insert(0, str(BASE_DIR))
