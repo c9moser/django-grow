b29bef49ed85db3b6a516b095231d40b2aa82da8
Christian Moser <christian@cmoser.eu>
2026-04-19 22:21:37 +0200

    2026.04.19 22:21:37 (cachyos)

--- core/admin.py
+++ core/admin.py
@@ -1,10 +1,13 @@
 from django.contrib import admin
-
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 from .models import User
 
 
 @admin.register(User)
-class UserAdmin(admin.ModelAdmin):
+class UserAdmin(BaseUserAdmin):
     list_display = ('email', 'username', 'first_name', 'last_name', 'is_staff')
     search_fields = ('email', 'username', 'first_name', 'last_name')
+    readonly_fields = ('last_login', 'date_joined')
+    fieldsets = BaseUserAdmin.fieldsets + (
+    )
     ordering = ('email',)
