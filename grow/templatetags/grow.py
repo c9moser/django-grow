17b0d91cd04f09aa35ca9cceb5bee94425633d91
Christian Moser <christian@cmoser.eu>
2026-04-27 18:20:25 +0200

    2026.04.27 18:20:25 (workstation)

--- grow/templatetags/grow.py
+++ grow/templatetags/grow.py
@@ -5,13 +5,13 @@ register = template.Library()
 
 
 @register.filter
+def grow_urlencode(value: str) -> str:
+    from urllib.parse import quote
+    return quote(value)
+
+
+@register.filter
 def grow_template(template_name: str) -> str:
     from grow.settings import GROW_TEMPLATES
 
     return GROW_TEMPLATES.get(template_name, template_name)
-
-
-@register.filter
-def grow_urlencode(value: str) -> str:
-    from urllib.parse import quote
-    return quote(value)
